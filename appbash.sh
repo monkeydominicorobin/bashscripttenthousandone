899eca577009b2141e23f356ce5bea9b515a0283
Rommel Dominguez <bm-2cu8dmto5ga5pf2xkvapdrqcpwtzybsxpw@riseup.net>
2021-09-20 18:09:25 +0800

    Create appbash.sh in a jiffy

---
+++ appbash.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+fbashone()
+{
+        echo "Presenting Function One"
+}
+fbashtwo()
+{
+        echo "I am Function Two"
+}
+fbashthree()
+{
+        echo "I am Function Three"
+}
+fbashone
+fbashtwo
+fbashthree
